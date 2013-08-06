2f42354f1bd38763cb6ab8b4977703adb1fa3aad
Senthil <msen2000@gmail.com>
2013-08-06 10:55:26 -0700

    HelloWorld calss added

---
+++ HelloWorld/src/org/test/HelloWorld.java
@@ -0,0 +1,23 @@
+package org.test;
+
+public class HelloWorld {
+
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
