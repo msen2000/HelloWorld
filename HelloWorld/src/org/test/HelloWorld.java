b819595b74947734ce549f2b4b1c6c7877324211
Senthil <msen2000@gmail.com>
2013-08-06 10:56:26 -0700

    hellowwrkd updated

--- HelloWorld/src/org/test/HelloWorld.java
+++ HelloWorld/src/org/test/HelloWorld.java
@@ -17,7 +17,8 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		HelloWorld hw= new HelloWorld();
+		hw.setName("Senthi");
 	}
 
 }
