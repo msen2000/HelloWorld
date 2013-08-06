236ff97218ab952343a41c62187388f32edb45bc
Senthil <msen2000@gmail.com>
2013-08-06 15:08:33 -0700

    Sample Prj1

---
+++ SamplePrj1/src/org/sample/SampleWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.sample;
+
+/**
+ * @author ca31085
+ *
+ */
+public class SampleWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
